d08f80aa806371c693b083ded717df48684d0b80
KiranBuzzaGH <knb34@cantab.ac.uk>
2025-07-06 23:34:23 +0100

    Comparing integers app

---
+++ java/misc/ComparingIntegers.java
@@ -0,0 +1,41 @@
+// Displays whether an integer and its square are greater than, less than or equal to 100.
+
+import java.util.Scanner;
+
+public class ComparingIntegers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int integer = input.nextInt();
+
+        int square = integer * integer;
+        int currentNumber = 100;
+
+        // Note that this is terrible design with lots of redundant if statements
+        if (integer == currentNumber) {
+            System.err.printf("%d == %d%n", integer, currentNumber);
+        }
+        if (integer > currentNumber) {
+            System.err.printf("%d > %d%n", integer, currentNumber);
+        }
+        if (integer < currentNumber) {
+            System.err.printf("%d < %d%n", integer, currentNumber);
+        }
+        if (integer != currentNumber) {
+            System.err.printf("%d != %d%n", integer, currentNumber);
+        }
+        if (square == currentNumber) {
+            System.err.printf("%d == %d%n", square, currentNumber);
+        }
+        if (square > currentNumber) {
+            System.err.printf("%d > %d%n", square, currentNumber);
+        }
+        if (square < currentNumber) {
+            System.err.printf("%d < %d%n", square, currentNumber);
+        }
+        if (square != currentNumber) {
+            System.err.printf("%d != %d%n", square, currentNumber);
+        }
+    }
+}
