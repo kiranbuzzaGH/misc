bf9791571ba4a36e1cc3e2a30863b7dccd420426
KiranBuzzaGH <knb34@cantab.ac.uk>
2025-07-17 14:02:56 +0100

    Abstracted away some functionality

--- java/Deitel-Java-HowToProgram/chapter-3/AccountTest.java
+++ java/Deitel-Java-HowToProgram/chapter-3/AccountTest.java
@@ -6,8 +6,8 @@ public class AccountTest {
         Account account1 = new Account("Jane Green", 50.00);
         Account account2 = new Account("John Blue", -7.53);
 
-        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%.2f%n%n", account2.getName(), account2.getBalance());
+        displayAccount(account1);
+        displayAccount(account2);
 
         Scanner input = new Scanner(System.in);
 
@@ -17,16 +17,16 @@ public class AccountTest {
         System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
         account1.deposit(depositAmount);
 
-        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
+        displayAccount(account1);
+        displayAccount(account2);
 
         System.out.print("Enter deposit amount for account2: ");
         depositAmount = input.nextDouble();
         System.out.printf("%nadding %.2f to account2 balance%n%n", depositAmount);
         account2.deposit(depositAmount);
 
-        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
+        displayAccount(account1);
+        displayAccount(account2);
 
         // Test withdraw method
         System.out.print("Enter withdrawal amount for account1: ");
@@ -34,17 +34,21 @@ public class AccountTest {
         System.out.printf("%ntaking %.2f from account1 balance%n%n", withdrawalAmount);
         account1.withdraw(withdrawalAmount);
 
-        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
+        displayAccount(account1);
+        displayAccount(account2);
 
         System.out.print("Enter withdrawal amount for account2: ");
         withdrawalAmount = input.nextDouble();
         System.out.printf("%ntaking %.2f from account2 balance%n%n", withdrawalAmount);
         account1.withdraw(withdrawalAmount);
 
-        System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
-        System.out.printf("%s balance: $%.2f%n", account2.getName(), account2.getBalance());
+        displayAccount(account1);
+        displayAccount(account2);
 
         input.close();
+    }
+
+    public static void displayAccount(Account accountToDisplay) {
+        System.out.printf("%s balance: $%.2f%n", accountToDisplay.getName(), accountToDisplay.getBalance());
     }
 }
